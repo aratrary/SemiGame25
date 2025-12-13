ede0de29926179073336dd9e0bb226450cac93c5
BlueSR10 <167279728+BlueSR10@users.noreply.github.com>
2025-12-14 01:14:47 +0900

    버퍼시간, 코요테시간 만듦

--- SMplay/Assets/Scripts/hyungJK/Moving.cs
+++ SMplay/Assets/Scripts/hyungJK/Moving.cs
@@ -11,6 +11,14 @@ public class Moving : MonoBehaviour
     public bool isJumping; //점프함?
     Rigidbody2D rigid;
     bool Ij; // 점프 인풋
+
+    public float bufferTime = 0.15f;
+    float bufferTimer;
+    bool Jumpkey = false; // 점프 실행시키는 트리거
+    public float coyoteTime = 0.1f;
+    float coyoteTimer;
+
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -23,17 +31,17 @@ public class Moving : MonoBehaviour
         if ((Iv != 0) || (rigid.linearVelocityX != 0))
             rigid.linearVelocityX = Iv * maxV;
             
-        if (Ij && !isJumping) // 키 누르는데 점프중 아님
+        if (Jumpkey) // 점프 받음
         {
+            Jumpkey = false;
             isJumping = true; // 공중에서 점프키 한 번 누르면 땅에 닫자마자 점프하는거 방지
-            if (isGround) 
-            {
-                rigid.AddForce(Vector2.up * StartJumppower, ForceMode2D.Impulse); //처음 점프
-            }
+            rigid.linearVelocityY = 0;
+            rigid.AddForce(Vector2.up * StartJumppower, ForceMode2D.Impulse); //처음 점프
+            coyoteTimer = 0;
         }
         else if (Ij && isJumping) // 키 누르는데 점프중
         {
-            Jpower = Mathf.Lerp(Jpower, 0, 0.1f); // 점프파워 0.1% 계속
+            Jpower = Mathf.Lerp(Jpower, 0, 0.1f); // 점프파워 10% 계속
             rigid.AddForce(Vector2.up * Jpower, ForceMode2D.Impulse);
             //Debug.Log(Jpower);
         }
@@ -49,16 +57,50 @@ public class Moving : MonoBehaviour
 
         Ij = Input.GetButton("Jump");
         //Debug.Log(Input.GetButton("Jump"));
+
+        if (Input.GetButtonDown("Jump")) // 키 누르는데 점프중 아님
+        {
+            if (coyoteTimer > 0)
+            {
+                Jumpkey = true;
+            }
+            else
+            {
+                bufferTimer = bufferTime;
+            }
+        }
+        if (bufferTimer > 0 && Input.GetButton("Jump")) // 점프키 누름 
+        {
+            bufferTimer -= Time.deltaTime; 
+            if (coyoteTimer > 0)
+            {
+                bufferTimer = 0;
+                Jumpkey = true;
+            }
+        }
+        if (!isGround && coyoteTimer > 0)
+        {
+            coyoteTimer -= Time.deltaTime;
+        }
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
+        if (coyoteTimer != coyoteTime)
+        {
+            coyoteTimer = coyoteTime;
+        }
+
+        isGround = true;
 
         if ((Jpower != Jumppower) && (!isJumping))
         {
             Jpower = Jumppower;
         }
-        isGround = true;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 
     void OnCollisionExit2D(Collision2D collision)
